a72ea78a519186471e1cdff40bac4659baba1550
Andrea Leon <andrea.leonaguilera@pcc.edu>
2020-10-16 04:13:21 -0700

    Initialize project

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Say Hello
+ *
+ * @author Andrea Leon
+ * @version 2020.22.09
+ */
+public class HelloWorld
+{   
+    /**
+     * Say hello to the world
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+}
