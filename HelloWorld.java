aaba9113765815f2b2d20895e5e4ce5794659ad0
Andrea Leon <andrea.leonaguilera@pcc.edu>
2020-10-16 04:30:48 -0700

    print date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 /**
  * Say Hello
@@ -8,5 +9,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
